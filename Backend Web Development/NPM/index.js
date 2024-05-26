c69a940c60a39e5bf720570be22d6d4eca52fcd6
Sameer Kadu <Samirkadu8@gmail.com>
2024-05-26 19:25:31 +0530

    lear about npm, install some packages like silyname, superheroes

---
+++ Backend Web Development/NPM/index.js
@@ -0,0 +1,7 @@
+// var generateName = require("sillyname");
+// var sillyname = generateName();
+// console.log(`my name is ${sillyname}.`);
+
+import { randomSuperhero } from 'superheroes';
+const name = randomSuperhero();
+console.log(`i am ${name}`);
