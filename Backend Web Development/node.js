913a0760a93a734b5b793bb811b035307ed40aea
Sameer Kadu <Samirkadu8@gmail.com>
2024-05-26 17:56:37 +0530

    basic file management with node js

--- Backend Web Development/node.js
+++ Backend Web Development/node.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
-fs.writeFile("massage.txt","Hello from sameer", (err) => {
+// fs.writeFile("massage.txt","Hello from sameer", (err) => {
+//     if(err) throw err;
+//     console.log("The file has been saved!");
+// });
+
+fs.readFile("./massage.txt", "utf8", (err , data) => {
     if(err) throw err;
-    console.log("The file has been saved!");
+    console.log(data);
 });
