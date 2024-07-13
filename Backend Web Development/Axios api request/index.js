a0d0598d5da4eb9e538eda564de3c498a655acc1
Sameer Kadu <Samirkadu8@gmail.com>
2024-07-13 21:01:57 +0530

    update

--- Backend Web Development/Axios api request/index.js
+++ Backend Web Development/Axios api request/index.js
@@ -11,20 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
-app.get("/", async (req, res) => {
-  try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
-    //console.log("Response received");
-    const result = response.data;
-    //console.log("Result:", result); 
-    res.render("index.ejs", { data: result });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
-});
+// app.get("/", async (req, res) => {
+//   try {
+//     const response = await axios.get("https://bored-api.appbrewery.com/random");
+//     //console.log("Response received");
+//     const result = response.data;
+//     //console.log("Result:", result); 
+//     res.render("index.ejs", { data: result });
+//   } catch (error) {
+//     console.error("Failed to make request:", error.message);
+//     res.render("index.ejs", {
+//       error: error.message,
+//     });
+//   }
+// });
 
 app.post("/", async (req, res) => {
  
